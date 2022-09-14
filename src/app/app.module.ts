cc824ce03820f6d2df42739f4a1faaaa0d9f81e3
Lucero Raul <luceroraul12@gmail.com>
2022-09-13 23:08:30 -0300

    add menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from 'primeng/api';
 
 import { AppComponent } from './app.component';
 
 //propios
 import { PrimeNGModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { PrimeNGModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PrimeNGModule],
   exports: [MenuComponent],
 })
 export class SharedModule {}
