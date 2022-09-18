2419f9408fbe9baa7097d6345f29cc4941de3fa3
Lucero Raul <luceroraul12@gmail.com>
2022-09-18 11:27:35 -0300

    number pipes

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,14 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumerosComponent implements OnInit {
+  public ventasNetas: number = 23984732694873245.34345;
+  public porcentaje: number = 0.045;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
