76269beee0c5a45065a2b5048422c70326a41263
Lucero Raul <luceroraul12@gmail.com>
2022-09-23 23:24:25 -0300

    add data de prueba

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class OrdenarComponent implements OnInit {
+  listaDeHeroes: heroe[] = [
+    {
+      nombre: 'Capitan América',
+      vuela: false,
+      color: Color.ROJO,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.VERDE,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.NEGRO,
+    },
+    {
+      nombre: 'Pikachú',
+      vuela: false,
+      color: Color.AMARILLO,
+    },
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.ROJO,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
